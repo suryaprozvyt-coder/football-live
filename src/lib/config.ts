8f09d1130e1e5e94e5fb5fb6e5a5975cce357dbd
suryaprozvyt-coder <suryaprozvyt@gmail.com>
2026-07-13 16:30:55 +0530

    Update livePassword to a new value

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -48,5 +48,5 @@ export const streamConfig = {
    * Password required to view the Live page. Change this to whatever you
    * want. Leave as an empty string ("") to turn password protection off.
    */
-  livePassword: "changeme",
+  livePassword: "26",
 };
