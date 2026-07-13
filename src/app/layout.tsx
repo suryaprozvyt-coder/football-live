c7f38257534335e551f297348ed090862ae2e565
suryaprozvyt-coder <suryaprozvyt@gmail.com>
2026-07-13 23:15:54 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,9 +70,18 @@ export default function RootLayout({
           src="https://www.highperformanceformat.com/d96301e33b263e8cb961725c6069cd78/invoke.js"
           strategy="afterInteractive"
         />
-        <Script
-          src="https://pl30350130.effectivecpmnetwork.com/75/e2/6e/75e26ed1b9aac8120a35c24b61e1a67a.js"
-          strategy="afterInteractive"
+        <script>
+  atOptions = {
+    'key' : '78d996c82d2e698731f371da4f5324d5',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/78d996c82d2e698731f371da4f5324d5/invoke.js"></script>
+
+
         />
         <main id="main-content" className="flex-1">
           {children}
